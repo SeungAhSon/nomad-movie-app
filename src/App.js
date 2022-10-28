3c4da1b56e9d782b1411198cd41b9fe7d3f781dd
minhyeok <santa4246@naver.com>
2022-10-28 14:38:48 +0900

    #12.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from "axios";
+import Movie from './Movie';
+
 //import PropTypes from 'prop-types';
 
 // Function component는 return
@@ -10,26 +13,39 @@ import React from 'react';
 // 왜냐면 setstate는 비동기로 처리되므로, 여러번 호출하거나 다른 곳에서 동시에 변경되는 경우 처리 순서가 바뀌어 오류가 생길 수 있음 ?
 // current.count 할 떄 괄호 기억하기!!! {}할 떄 return 인지 객채인지 구분하기 위해서 ({})
 
+// #11.3 처음에 render를 하면 호출되는 life cycle method = componentDidMount
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState(current => ({ count : current.count + 1 }));
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading:false})
   };
 
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
+  async componentDidMount() {
+    this.getMovies();
+  }
 
   render() {
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+    const { isLoading, movies } = this.state;
+    return (<div>
+      {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                />
+                )
+          )}
       </div>
     );
   }
